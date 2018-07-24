835ee7eb24613163c4643255d59455bb328c5e76
wellyandriani <wellyandriani97@gmail.com>
2018-07-24 17:28:35 +0700

    add data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+[
+    {
+   "id": 1,
+"Name" : "Luke Skywalker",
+"Height" : "173", 
+"Hair Color" : "pink"
+},
+
+{
+    "id": 2,
+ "Name" : "Luke Skywalke",
+ "Height" : "174", 
+ "Hair Color" : "blond"
+ },
+ {
+    "id": 3,
+ "Name" : "Luke Skywalk",
+ "Height" : "175", 
+ "Hair Color" : "blue"
+ },
+ {
+    "id": 4,
+ "Name" : "Luke Skywal",
+ "Height" : "176", 
+ "Hair Color" : "borwn"
+ },
+ {
+    "id": 5,
+ "Name" : "Luke Skywa",
+ "Height" : "177", 
+ "Hair Color" : "black"
+ }
+]
